2025efab66e9a3650e83f30b447721cad21f7081
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2015-11-24 09:53:16 +0100

    dump-data.sh

--- tools/dump-data.sh
+++ tools/dump-data.sh
@@ -4,7 +4,7 @@ for i in massif circuit place ; do
   ./bin/importer --write-geojson=share/WebApplication/static/data/bleau-${i}s-geoson.js --js-var=${i}_geojson --${i}s data/bleau.json
 done
 
-./bin/importer --write-gpx=bleau.gpx --massifs --circuits --places data/bleau.json
+./bin/importer --write-gpx=data/bleau.gpx --massifs --circuits --places data/bleau.json
 
 ####################################################################################################
 #
